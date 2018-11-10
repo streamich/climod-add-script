1038f75e940344c00779c806ab67a7f0b763e82b
streamich <streamich@gmail.com>
2018-11-10 13:19:06 +0100

    feat: 🎸 implement first version

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const {packageJson} = require('mrm-core');
+const argv = require('minimist')(process.argv.slice(2));
+const {name, cmd} = argv;
+
+if ((typeof name !== 'string') || !name) {
+  throw new TypeError('climod-add-script script name not specified.');
+} else if (typeof cmd !== 'string') {
+  throw new TypeError('climod-add-script command not specified.');
+}
+
+packageJson().setScript(name, cmd).save();
